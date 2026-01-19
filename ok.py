62c6782b14ed4c720a19dc86acd1276997681016
dushniy-11 <den.kalimov@gmail.com>
2026-01-19 23:03:52 +0300

    Update ok.py

--- ok.py
+++ ok.py
@@ -2,16 +2,15 @@ import arcade
 import random
 
 
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 500
 SCREEN_TITLE = "DиноZаврик"
-GAME_SPEED = 5
-PLAYER_SPEED = 5
+PLAYER_SPEED = 10
 JUMP_SPEED = 300
 PLAYER_SCALE = 0.3
 COLLISION_SCALE = 0.4
 GRAVITY = 9
-CAMERA_LERP = 0.11
+CAMERA_LERP = 0.15
 
 
 class Cactus(arcade.Sprite):
@@ -20,11 +19,16 @@ class Cactus(arcade.Sprite):
         self.center_x = x
         self.center_y = y
 
+class Pterodactyle(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__("pterodactyl.png", scale=COLLISION_SCALE)
+        self.center_x = x
+        self.center_y = y
 
 class Dyno(arcade.Sprite):
     def __init__(self):
         super().__init__("dyno.png", scale=PLAYER_SCALE)
-        self.center_x = 300
+        self.center_x = 0
         self.center_y = 30
         self.speed_x = PLAYER_SPEED
         self.speed_y = 0
@@ -32,6 +36,8 @@ class Dyno(arcade.Sprite):
     def update(self, delta_time, keys):
         self.change_x = self.speed_x
         self.change_y = self.speed_y
+        if self.speed_x < 30:
+            self.speed_x += 0.003
 
 
 class MyGame(arcade.Window):
@@ -44,9 +50,8 @@ class MyGame(arcade.Window):
     def setup(self):
         self.keys_pressed = set()
         self.players_list = arcade.SpriteList()
-        self.cactus_list = arcade.SpriteList()
-        self.background_list = arcade.SpriteList()
         self.collisions_list = arcade.SpriteList()
+        self.background_list = arcade.SpriteList()
         self.player = Dyno()
         self.players_list.append(self.player)
 
@@ -73,7 +78,7 @@ class MyGame(arcade.Window):
         self.clear()
         self.background_list.draw()
         self.players_list.draw()
-        self.cactus_list.draw()
+        self.collisions_list.draw()
         self.world_camera.use()
 
     def on_update(self, delta_time):
@@ -81,8 +86,9 @@ class MyGame(arcade.Window):
             return
         self.engine.update()
         self.players_list.update(delta_time, self.keys_pressed)
-        self.cactus_list.update()
+        self.collisions_list.update()
         self.make_cactus()
+        self.make_pterodactyl()
 
         position = (
             self.player.center_x,
@@ -94,15 +100,15 @@ class MyGame(arcade.Window):
             CAMERA_LERP,
         )
 
-        for element in self.cactus_list:
+        for element in self.collisions_list:
             if arcade.check_for_collision(self.player, element):
                 self.end_game = True
-            if element.center_x < self.player.center_x - (SCREEN_WIDTH // 2 + 200):
+            if element.center_x < self.player.center_x - (SCREEN_WIDTH // 2 + SCREEN_WIDTH // 2):
                 element.remove_from_sprite_lists()
 
         for element in self.background_list:
             max_x = max(object.center_x for object in self.background_list)
-            if element.center_x < self.player.center_x - (SCREEN_WIDTH // 2 + 620):
+            if element.center_x < self.player.center_x - (SCREEN_WIDTH // 2 + SCREEN_WIDTH // 1.5):
                 element.center_x = max_x + 1150
 
     def on_key_press(self, key, modifiers):
@@ -111,11 +117,20 @@ class MyGame(arcade.Window):
                 self.player.change_y = JUMP_SPEED
 
     def make_cactus(self):
-        if len(self.cactus_list) < 1:
-            x = self.player.center_x + 500
-            y = 218
-            cactus = Cactus(x, y)
-            self.cactus_list.append(cactus)
+        if len(self.collisions_list) < 1:
+            if random.random() > 0.4:
+                x = self.player.center_x + (100 * random.randint(4, 8))
+                y = 210
+                cactus = Cactus(x, y)
+                self.collisions_list.append(cactus)
+
+    def make_pterodactyl(self):
+        if len(self.collisions_list) < 1:
+            if random.random() < 0.4:
+                x = self.player.center_x + (100 * random.randint(4, 8))
+                y = self.player.center_y + (40 * random.randint(1, 3))
+                pterodactyle = Pterodactyle(x, y)
+                self.collisions_list.append(pterodactyle)
 
 def main():
     window = MyGame()
